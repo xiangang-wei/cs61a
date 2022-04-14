0b716f9750aea758886a4409d77c9635cbd7ed08
xiangangwei <xiangangwei@tencent.com>
2022-04-14 17:14:15 +0800

    initialize project

---
+++ hw/hw04/hw04.py
@@ -0,0 +1,233 @@
+from math import sqrt
+from re import A
+HW_SOURCE_FILE = 'hw04.py'
+
+###############
+#  Questions  #
+###############
+
+def intersection(st, ave):
+    """Represent an intersection using the Cantor pairing function."""
+    return (st+ave)*(st+ave+1)//2 + ave
+
+def street(inter):
+    return w(inter) - avenue(inter)
+
+def avenue(inter):
+    return inter - (w(inter) ** 2 + w(inter)) // 2
+
+w = lambda z: int(((8*z+1)**0.5-1)/2)
+
+def taxicab(a, b):
+    """Return the taxicab distance between two intersections.
+
+    >>> times_square = intersection(46, 7)
+    >>> ess_a_bagel = intersection(51, 3)
+    >>> taxicab(times_square, ess_a_bagel)
+    9
+    >>> taxicab(ess_a_bagel, times_square)
+    9
+    """
+    return abs(street(a) - street(b))+abs(avenue(a) - avenue(b))
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    res=[]
+    length = len(s)
+    i=0
+    while i<length:
+        sqrt_val = sqrt(s[i])
+        if round(sqrt_val) == sqrt_val:
+            res.append(round(sqrt_val))
+        i+=1
+    return res
+
+def g(n):
+    """Return the value of G(n), computed recursively.
+
+    >>> g(1)
+    1
+    >>> g(2)
+    2
+    >>> g(3)
+    3
+    >>> g(4)
+    10
+    >>> g(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g', ['While', 'For'])
+    True
+    """
+    if n <= 3:
+        return n
+    else:
+        return g(n-1)+2*g(n-2)+3*g(n-3)
+
+def g_iter(n):
+    """Return the value of G(n), computed iteratively.
+
+    >>> g_iter(1)
+    1
+    >>> g_iter(2)
+    2
+    >>> g_iter(3)
+    3
+    >>> g_iter(4)
+    10
+    >>> g_iter(5)
+    22
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'g_iter', ['Recursion'])
+    True
+    """
+    if n <=3:
+        return n
+    else:
+        a,b,c = 3,2,1
+        i=4
+        while i<=n:
+            a,b,c = a+2*b+3*c,a,b 
+            i+=1
+        return a
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(7)
+    7
+    >>> pingpong(8)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    0
+    >>> pingpong(30)
+    6
+    >>> pingpong(68)
+    2
+    >>> pingpong(69)
+    1
+    >>> pingpong(70)
+    0
+    >>> pingpong(71)
+    1
+    >>> pingpong(72)
+    0
+    >>> pingpong(100)
+    2
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'pingpong', ['Assign', 'AugAssign'])
+    True
+    """
+    def sum(n):
+        if n == 1:
+            return 1
+        else:
+            if switch(n):
+                return sum(n-1)+1
+            else:
+                return sum(n-1)-1
+
+    def switch(n):
+        if n <= 7:
+            return True
+        else:
+            if has_seven(n-1) or (n-1)%7 == 0:
+                return not switch(n-1)
+            else:
+                return switch(n-1)
+
+    return sum(n)
+
+    
+
+def has_seven(k):
+    """Returns True if at least one of the digits of k is a 7, False otherwise.
+
+    >>> has_seven(3)
+    False
+    >>> has_seven(7)
+    True
+    >>> has_seven(2734)
+    True
+    >>> has_seven(2634)
+    False
+    >>> has_seven(734)
+    True
+    >>> has_seven(7777)
+    True
+    """
+    if k % 10 == 7:
+        return True
+    elif k < 10:
+        return False
+    else:
+        return has_seven(k // 10)
+
+def count_change(amount):
+    """Return the number of ways to make change for amount.
+
+    >>> count_change(7)
+    6
+    >>> count_change(10)
+    14
+    >>> count_change(20)
+    60
+    >>> count_change(100)
+    9828
+    """
+    def pow(i):
+        if i == 0:
+            return 1
+        else:
+            return 2*pow(i-1)
+
+    def get_next(m):
+        if m < 1:
+            return 0
+        else:
+            i=0
+            while pow(i)<=m:
+                i+=1
+            return pow(i-1)
+    
+    def get_partition(n,m):
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 0
+        elif m <= 0:
+            return 0
+        else:
+            return get_partition(n-m,m)+get_partition(n,m//2)
+
+    return get_partition(amount,get_next(amount))
+
+###################
+# Extra Questions #
+###################
+
+from operator import sub, mul
+
+def make_anonymous_factorial():
+    """Return the value of an expression that computes factorial.
+
+    >>> make_anonymous_factorial()(5)
+    120
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'make_anonymous_factorial', ['Assign', 'AugAssign', 'FunctionDef', 'Recursion'])
+    True
+    """
+    return 'YOUR_EXPRESSION_HERE'
---
+++ lab/lab03/lab03_extra.py
@@ -0,0 +1,143 @@
+""" Optional problems for Lab 3 """
+
+from lab03 import *
+
+## Higher order functions
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    def my_cycle(n):
+        def s(x):
+            i=0
+            while i < n:
+                if i % 3 == 0:
+                    x = f1(x)
+                elif i % 3 ==1:
+                    x = f2(x)
+                else:
+                    x = f3(x)
+                i+=1
+            return x
+        return s
+    return my_cycle
+## Lambda expressions
+
+def is_palindrome(n):
+    """
+    Fill in the blanks '_____' to check if a number
+    is a palindrome.
+
+    >>> is_palindrome(12321)
+    True
+    >>> is_palindrome(42)
+    False
+    >>> is_palindrome(2015)
+    False
+    >>> is_palindrome(55)
+    True
+    """
+    x, y = n, 0
+    f = lambda: 10*y + x%10
+    while x > 0:
+        x, y = x//10, f()
+    return y == n
+
+## More recursion practice
+
+def skip_mul(n):
+    """Return the product of n * (n - 2) * (n - 4) * ...
+
+    >>> skip_mul(5) # 5 * 3 * 1
+    15
+    >>> skip_mul(8) # 8 * 6 * 4 * 2
+    384
+    """
+    if n == 2:
+        return 2
+    elif n == 1:
+        return 1
+    else:
+        return n * skip_mul(n - 2)
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    def recursion(x):
+        if x == 1:
+            return True
+        if n%x == 0:
+            return False
+        else:
+            return recursion(x-1)
+    return recursion(n-1)
+
+
+def interleaved_sum(n, odd_term, even_term):
+    """Compute the sum odd_term(1) + even_term(2) + odd_term(3) + ..., up
+    to n.
+
+    >>> # 1 + 2^2 + 3 + 4^2 + 5
+    ... interleaved_sum(5, lambda x: x, lambda x: x*x)
+    29
+    """
+    if n ==0:
+        return 0
+    elif n % 2 == 0:
+        return interleaved_sum(n-1,odd_term,even_term)+even_term(n)
+    else:
+        return interleaved_sum(n-1,odd_term,even_term)+odd_term(n)
+
+def ten_pairs(n):
+    """Return the number of ten-pairs within positive integer n.
+
+    >>> ten_pairs(7823952)
+    3
+    >>> ten_pairs(55055)
+    6
+    >>> ten_pairs(9641469)
+    6
+    """
+    total=0
+    def count_ten(x,n):
+        nonlocal total
+        if n == 0:
+            return
+        else:
+            if x + n%10 == 10:
+                total+=1
+            return count_ten(x,n//10)
+    while n > 0:
+        count_ten(n%10,n//10)
+        n=n//10
+    return total

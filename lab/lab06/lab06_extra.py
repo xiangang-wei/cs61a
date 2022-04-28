c11e413f9953062e2923b3969056aa14d9643509
xiangangwei <xiangangwei@tencent.com>
2022-04-29 00:02:38 +0800

    complete lab06

---
+++ lab/lab06/lab06_extra.py
@@ -0,0 +1,31 @@
+""" Optional problems for Lab 6 """
+
+## Nonlocal practice
+# from msilib import sequence
+
+
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    index = 0
+    def vender():
+        nonlocal index
+        res = snacks[index]
+        index = (index + 1) % len(snacks)
+        return res
+    return vender
